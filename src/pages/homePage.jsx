308fe484b51daac7bb4bbc42ee5a0017a974551f
Omar D. Sampayo <omar.1993.ds@hotmail.com>
2024-06-26 18:03:11 -0600

    ready

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 export default function homePage() {
     return (
         <div>
-            <h1>Home Page</h1>
-            <p>This is the home page</p>
-            <div>
-                <Link to="/login">Login</Link>
-                <Link to="/productos">Productos</Link>
-                <Link to="/producto/123">Producto 123</Link>
+            <h1 className="text-center p-10 font-bold text-xl">Bienvenido a Mercado Falso! Somos más falsos que las promesas de tu ex 😁</h1>
+            <div className="flex justify-center items-center w-full flex-col gap-4 min-h-dvh">
+                <section className="flex flex-col gap-4 rounded-md items-center bg-white/50 max-w-sm w-full">
+                    <Link className="hover:text-red-800 font-semibold" to="/login">Iniciar Sesión</Link>
+                    <Link className="hover:text-red-800 font-semibold" to="/productos">Ve los productos</Link>
+
+                </section>
             </div>
         </div>
     )
